b59c88d4d52eb270fb9aca8171905872bc26b97e
PaulMazeau <paul@ensemble.ooo>
2024-09-20 12:27:00 +0200

    remove test schema

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -5,7 +5,6 @@ import contact from "./singletonContact"
 import clients from "./clients"
 import singletonAbout from "./singletonAbout"
 import manifesteScene from "./manifesteScene"
-import testAjout from "./singletonTestAjout"
 
 export const schemaTypes = [
   manifest,
@@ -14,6 +13,5 @@ export const schemaTypes = [
   contact,
   singletonAbout,
   manifesteScene,
-  testAjout,
   clients,
 ]
